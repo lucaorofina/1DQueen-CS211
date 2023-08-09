49a988d1d8794246e769ca116fe2510e13ca4363
lucaorofina <85538390+lucaorofina@users.noreply.github.com>
2023-08-09 15:40:23 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1dQueen
+//
+//  Created by Luca Orofina on 2/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
